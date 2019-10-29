468e43d1bbc91060ca4c68f3227ebc574312dffc
Akshay <akjags@gmail.com>
2019-10-28 18:16:56 -0700

    Adding softmax results

--- code/model_behavior.py
+++ code/model_behavior.py
@@ -4,7 +4,6 @@ import os
 from sklearn.model_selection import train_test_split, cross_val_predict, cross_validate
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
-
 ### Function to load behavioral data from a .mat file
 def load_behavioral_data(filepath):
     bd = loadmat(filepath)
@@ -21,6 +20,7 @@ def load_behavioral_data(filepath):
 ### Function to compute the distance between features on each trial, given a particular model layer.
 def compute_feature_distance(obs_lay, obs_rf, bd, feature_dir):
     feature_distance = np.zeros((bd['nTrials'],))
+    trial_feat_dist = np.zeros((3, bd['nTrials']))
     for i in range(bd['nTrials']):
         imgName = bd['imNames'][bd['imgFam'][i]-1]
         
@@ -49,11 +49,15 @@ def compute_feature_distance(obs_lay, obs_rf, bd, feature_dir):
         trial_featDist = np.array([1-np.corrcoef(odd_feat.ravel(), std_feat1.ravel())[0,1],
                           1-np.corrcoef(odd_feat.ravel(), std_feat2.ravel())[0,1],
                           1-np.corrcoef(std_feat1.ravel(), std_feat2.ravel())[0,1]])
+
+        trial_feat_dist[:,i] = trial_featDist
+        # Calculate how far each image is, on average, from the other two images.
+        #image_separation = [np.mean(trial_featDist[0:-1]), np.mean(trial_featDist[[0,2]]), np.mean(trial_featDist[[1:]])]
         feature_distance[i] = np.mean(trial_featDist[:2])
 
         if i % 1000 == 0:
             print i, feature_distance[i]
-    return feature_distance
+    return trial_feat_dist
         
 ### Function to compute the log loss between the true labels and predicted values.
 def get_log_loss(yt, yp):
@@ -63,6 +67,7 @@ def get_log_loss(yt, yp):
 def fit_logistic_regression(bd, obs_lays, obs_rfs, feature_distance):
     valid = np.logical_not(np.isnan(bd['correct']))
     loss = np.zeros((len(obs_lays),len(obs_rfs)))
+    cv_preds = np.zeros((len(obs_lays), len(obs_rfs), np.sum(valid)))
     for i, obs_lay in enumerate(obs_lays):
         for j, obs_rf in enumerate(obs_rfs):
             feat_dist = feature_distance['{}_{}'.format(obs_rf, obs_lay)]
@@ -71,9 +76,34 @@ def fit_logistic_regression(bd, obs_lays, obs_rfs, feature_distance):
             logReg = LogisticRegression(class_weight='balanced')
             y_pred = cross_val_predict(logReg, feat_dist[valid].reshape(-1,1), bd['correct'][valid], cv=10, method='predict_proba')
             loss[i,j] = -np.sum(get_log_loss(bd['correct'][valid], y_pred[:,1]))
+            cv_preds[i,j,:] = y_pred[:,1]
 
             print('{0} {1} Model: LogLoss={2:.3f}'.format(obs_rf, obs_lay, loss[i,j]))
-    return loss
+    return loss, cv_preds, bd['correct'][valid]
+
+##### Function to compute softmax
+def softmax(X, axis=None):
+    if axis is None:
+        return np.divide(np.exp(X), np.sum(np.exp(x)))
+    else:
+        return np.divide(np.exp(X), np.sum(np.exp(X), axis=axis))
+
+##### Function to transform the feature distances into choice probabilities using a softmax
+def compute_softmax_choice_probability(feature_distance):
+    # 1. Convert pairwise distances into mean separation for each image.
+    image_distance = np.zeros(feature_distance.shape)
+    image_distance[0,:] = np.mean(feature_distance[[0,1],:], axis=0) # Oddity
+    image_distance[1,:] = np.mean(feature_distance[[0,2],:], axis=0) # Non-odd #1
+    image_distance[2,:] = np.mean(feature_distance[[1,2],:], axis=0) # Non-odd #2
+
+    # 2. Pass through a Gaussian kernel.
+    
+    # 3. Softmax to get each choice probability.
+    softmax_choice_probs = softmax(image_distance, axis=0)
+    model_choices = np.argmax(softmax_choice_probs, axis=0)
+
+    return softmax_choice_probs, model_choices
+
 
 if __name__ == "__main__":
     # 1. Load behavioral data
@@ -81,7 +111,7 @@ if __name__ == "__main__":
     wd = '/home/users/akshayj/texOdd'
     bd = load_behavioral_data('{}/data/{}_behavior.mat'.format(wd, subj))
 
-   # 2. Compute feature distance
+    # 2. Compute feature distance
     feature_dir = '/scratch/groups/jlg/gram_texOB'
     obs_lays = ['conv1_1', 'pool1', 'pool2', 'pool4']
     obs_rfs = ['1x1', '2x2', '3x3', '4x4', '5x5', '6x6']
@@ -105,6 +135,15 @@ if __name__ == "__main__":
                 print('{} already computed; skipping...'.format(key)) 
 
     # 3. Fit logistic regression
-    loss = fit_logistic_regression(bd, obs_lays, obs_rfs, feature_distance)
-    np.save('{}/results/{}_log_loss.npy'.format(wd, subj), loss)
+    results = {'obs_lays': obs_lays, 'obs_rfs': obs_rfs}
+    for obs_rf in obs_rfs:
+        for obs_lay in obs_lays:
+            key = '{}_{}'.format(obs_rf, obs_lay)
+            softmax_choice_prob, model_choice = compute_softmax_choice_probability(feature_distance[key])
+            results[key] = {'softmax_choice_prob':softmax_choice_prob, 'model_choice': model_choice}
+    np.save('{}/results/{}_softmax_model.npy'.format(wd, subj), results)
+    print('Saved softmax results to {}/results'.format(wd))
+    #results['loss'], results['predictions'], results['responses'] = fit_logistic_regression(bd, obs_lays, obs_rfs, feature_distance)
+    #np.save('{}/results/{}_prediction_results.npy'.format(wd, subj), results)
+
 
